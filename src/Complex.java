fa0665cae1c8a478dbedd75c054d03b5f69fb8bb
Paulina <paulina.winkowska@gmail.com>
2015-03-05 23:44:37 +0100

    Division operation

--- src/Complex.java
+++ src/Complex.java
@@ -2,5 +2,63 @@
  * Created by Hubert on 2015-03-05.
  */
 public class Complex{
+    private double Re;
+    private double Im;
 
+    public Complex (double Re, double Im) {
+        this.Re = Re;
+        this.Im = Im;
+    }
+
+    public Double getRe() {
+        return this.Re;
+    }
+
+    public Double getIm() {
+        return this.Im;
+    }
+
+
+    public Complex divide(double addedRe, double addedIm) {
+        if (addedRe == 0 && addedIm ==0) {
+            throw new IllegalArgumentException();
+        } else {
+            double newRe = 0;
+            double newIm = 0;
+
+            newRe = (Re * addedRe + Im * addedIm) / (Math.sqrt(addedRe) + Math.sqrt(addedIm));
+            newIm = (Im * addedRe - Re * addedIm) / (Math.sqrt(addedRe) + Math.sqrt(addedIm));
+
+            return new Complex(newRe, newIm);
+        }
+    }
+
+    public Complex divide(Complex other) {
+        return divide(other.getRe(), other.getIm());
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Complex complex = (Complex) o;
+
+        if (Double.compare(complex.Im, Im) != 0) return false;
+        if (Double.compare(complex.Re, Re) != 0) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        temp = Double.doubleToLongBits(Re);
+        result = (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(Im);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
 }
--- test/ComplexTest.java
+++ test/ComplexTest.java
@@ -1,25 +1,54 @@
-import junit.framework.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class ComplexTest {
+
+
 
     @Test
     public void addTest() throws Exception {
         Complex c = new Complex(10, 10);
-        assertEquals(new Complex(10, 12), c.add(0, 2));
-        assertEquals(new Complex(10, 12), c.add(new Complex(0, 2)));
-        assertEquals(new Complex(8.5, 8.5), c.add(new Complex(-1.5, -1.5));
+//        assertEquals(new Complex(10, 12), c.add(0, 2));
+//        assertEquals(new Complex(10, 12), c.add(new Complex(0, 2)));
+//        assertEquals(new Complex(8.5, 8.5), c.add(new Complex(-1.5, -1.5)));
     }
 
     @Test
     public void subtractTest() throws Exception {
         Complex c = new Complex(10, 10);
-        assertEquals(new Complex(10, 8), c.minus(0, 2));
-        assertEquals(new Complex(10, 8), c.minus(new Complex(0, 2)));
-        assertEquals(new Complex(11.5, 11.5), c.minus(new Complex(-1.5, -1.5));
+//        assertEquals(new Complex(10, 8), c.minus(0, 2));
+//        assertEquals(new Complex(10, 8), c.minus(new Complex(0, 2)));
+//        assertEquals(new Complex(11.5, 11.5), c.minus(new Complex(-1.5, -1.5)));
 
     }
 
+    @Test
+    public void DivisionTest() throws Exception {
+        Complex c1 = new Complex(10, 10);
+        assertEquals(new Complex(5, 5), c1.divide(2, 0));
+        assertEquals(new Complex(5, 5), c1.divide(new Complex(2, 0)));
+
+        assertEquals(new Complex(-5, 5), c1.divide(0, 2));
+        assertEquals(new Complex(-5, 5), c1.divide(new Complex(0, 2)));
+
+        assertEquals(new Complex(2.5, 2.5), c1.divide(4, 0));
+        assertEquals(new Complex(2.5, 2.5), c1.divide(new Complex(4, 0)));
+
+        Complex c2 = new Complex(3, 6);
+        assertEquals(new Complex(2.25, 0.75), c2.divide(2, 2));
+        assertEquals(new Complex(2.25, 0.75), c2.divide(new Complex(2, 2)));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void DivisionByComplexZeroAsNumbersTest() {
+        Complex c = new Complex(10, 10);
+        c.divide(0, 0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void DivisionByComplexZeroAsComplexTest() {
+        Complex c = new Complex(10, 10);
+        c.divide(new Complex(0, 0));
+    }
 }
