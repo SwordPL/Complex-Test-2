d55c7d4e29dce623d5fce478c184604bd1cebb16
Zuchens <zuchens@interia.pl>
2015-03-06 11:26:17 +0100

    new and better argument

--- src/Complex.java
+++ src/Complex.java
@@ -1,8 +1,21 @@
 
 public class Complex{
-    public double argument(){
+    public double argument() throws Exception {
+        double arg;
+        if(getRe()==0 && getIm()==0) throw new Exception();
+        if(getRe()==0) {
+            if (getIm() > 0) {
+                arg = Math.PI / 2;
+                return arg;
+            }
+            else {
+                arg = -1 * Math.PI / 2;
+                return arg;
+            }
+        }
+
         double tan = (getIm()/(getRe());
-        double arg = Math.atan(tan));
+         arg = Math.atan(tan);
         return arg;
     }
 }
